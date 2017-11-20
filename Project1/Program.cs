82e16ca68c987ff703be556e8a7ec66d952d90a5
Breuer Viktor <breuer_viktor@hotmail.com>
2017-11-20 16:38:58 +0100

    a változás csak annyit tartalmaz, hogy magyarázó szöveg került a kódba

--- Project1/Program.cs
+++ Project1/Program.cs
@@ -6,9 +6,12 @@ namespace Project1
     {
         public static void Main(string[] args)
         {
+            // ez a program rész kiírja a nevem
             Console.WriteLine("Breuer Viktor");
+            // ez a program rész definiálja a változókat
             int a = 1;
             int b = 2;
+            // ez a programrész kiírja a változók összeadott értékét
             Console.WriteLine(a + b);
         }
     }
